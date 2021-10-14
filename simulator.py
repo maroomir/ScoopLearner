ea3b46b40fc19f9acf94f7a933611932e8ba6bbc
Maroomir Yoon <maroomir@gmail.com>
2021-10-14 12:13:30 +0900

    Adjust the gym.Env

--- simulator.py
+++ simulator.py
@@ -1,64 +1,135 @@
+import math
 import random
 import time
 
 import matplotlib.pyplot
 import numpy
+from numpy import ndarray
+import gym
 
 
-class Simulator:
+class Simulator(gym.Env):
     def __init__(self,
+                 target: float,
                  src_path: str,
-                 tool_path: str):
-        self.ice_cream = numpy.loadtxt(src_path, delimiter=',', dtype=numpy.float32, encoding='utf-8-sig')
-        self.scooper = numpy.loadtxt(tool_path, delimiter=',', dtype=numpy.float32, encoding='utf-8-sig')
-        self.weight = 0
+                 tool_path: str,
+                 verbose=False):
+        # Construct the source and tool
+        self.source = numpy.loadtxt(src_path, delimiter=',', dtype=numpy.float32, encoding='utf-8-sig')
+        self.tool = numpy.loadtxt(tool_path, delimiter=',', dtype=numpy.float32, encoding='utf-8-sig')
+        assert isinstance(self.source, ndarray) and isinstance(self.tool, ndarray)
+        self.pipeline = self.source.copy()
+        # Construct the target count and weight
+        self.cup_weight = 0
+        self.target_weight = target
+        # Calculate the expected scoop weight and iteration times
+        source_count = math.ceil(self.source.size / self.tool.sum())
+        self.target_scoop = self.source.sum() / source_count
+        # Bring-up the other parameters
+        self.verbose = verbose
+        self.step_count = 0
+        self.episode_count = 0
+        self.cmd_count = self.source.shape[1] - self.tool.shape[1]
+        self.done = False
 
-    # gravity module
-    def __gravity__(self):
-        src_h, src_w = self.ice_cream.shape
-        assert src_w != 0 and src_h != 0
+    # Called at the beginning of each episode
+    def reset(self):
+        assert isinstance(self.source, ndarray)
+        if self.verbose:
+            print("-------------")
+            print(self.episode_count, "cups")
+        self.step_count = 0
+        self.cup_weight = 0
+        self.done = False
+        self.episode_count += 1
+        return self._get_obs()
+
+    # Adjust the gravity at the source
+    def __gravity(self):
+        source_height, source_width = self.pipeline.shape
+        assert source_width != 0 and source_height != 0
         lines = None
-        for x in range(0, src_w):
-            line = self.ice_cream[:, x]
-            mass = line[line > 0]
-            empty = numpy.zeros(src_h - mass.shape[0])
-            line = numpy.concatenate([empty, mass], axis=0).reshape((src_h, 1))
+        for x in range(0, source_width):
+            line = self.pipeline[:, x]
+            mass_line = line[line > 0]
+            empty_line = numpy.zeros(source_height - mass_line.shape[0])
+            line = numpy.concatenate([empty_line, mass_line], axis=0).reshape((source_height, 1))
             lines = line if lines is None else numpy.hstack([lines, line])
-        assert lines.shape != src_h, src_w
+        assert lines.shape != source_height, source_width
         return lines
 
-    def __call__(self, pos: tuple):
-        tool_h, tool_w = self.scooper.shape
-        src_h, src_w = self.ice_cream.shape
-        assert src_w != 0 and src_h != 0
-        x, y = pos
-        assert x + tool_w <= src_w and y + tool_h <= src_h
-        # Scoop the ice-cream
-        scoop = self.ice_cream[y:y + tool_h, x:x + tool_w] * self.scooper
-        # Measure the weight of scoop
-        self.weight = scoop.sum()
-        # Remove as the mass of scoop
-        self.ice_cream[y:y + tool_h, x:x + tool_w] -= scoop
-        # Adjust the gravity module
-        self.ice_cream = self.__gravity__()
-        return self.weight
+    # Replace the source
+    def __replace(self):
+        if self.verbose:
+            print("replace the ice-cream")
+        return self.source.copy()
 
-    def inspection(self):
-        tool_h, tool_w = self.scooper.shape
-        src_h, src_w = self.ice_cream.shape
-        assert src_w != 0 and src_h != 0
+    # Calculate the candidate points
+    def inspect(self):
+        tool_height, tool_width = self.tool.shape
+        source_height, source_width = self.pipeline.shape
+        assert source_width != 0 and source_height != 0
+        verifiers = list()
         candidates = list()
-        for x in range(0, (src_w - tool_w) + 1):
-            line = self.ice_cream[:, x]
-            for y in range(0, (src_h - tool_h) + 1):
+        for x in range(0, (source_width - tool_width) + 1):
+            line = self.pipeline[:, x]
+            for y in range(0, (source_height - tool_height) + 1):
                 if line[y] != 0:
                     candidates.append((x, y))
+                    verifiers.append(True)
                     break
-        return candidates
+                elif y == source_height - tool_height:
+                    candidates.append((x, y))
+                    verifiers.append(False)
+        return candidates, verifiers
+
+    def _get_obs(self):
+        return self.pipeline
+
+    def _get_reward(self, epsilon=0.5):
+        # Put the ice cream close to target weight as few times as possible
+        if self.done:
+            weight_score = self.target_weight / (self.cup_weight - self.target_weight + epsilon)
+            time_score = - (2 ** self.step_count)
+            return weight_score + time_score
+        else:
+            return self.cup_weight / self.step_count - self.target_scoop
+
+    def _get_info(self):
+        return {'total_weight': self.cup_weight, 'step_count': self.step_count}
+
+    def step(self, action: list):
+        tool_height, tool_width = self.tool.shape
+        source_height, source_width = self.pipeline.shape
+        # Get a position from the action command
+        assert source_width != 0 and source_height != 0
+        pos_candidates, is_verifiers = self.inspect()
+        x, y = pos_candidates[action[0]]
+        assert x + tool_width <= source_width and y + tool_height <= source_height
+        # Scoop the ice-cream
+        scoop = self.pipeline[y:y + tool_height, x:x + tool_width] * self.tool
+        # Measure the weight of scoop
+        self.cup_weight += scoop.sum()
+        # Remove the ice-cream as the scoop
+        self.pipeline[y:y + tool_height, x:x + tool_width] -= scoop
+        self.pipeline = self.__gravity()
+        self.step_count += 1
+        # Stop the episode when the cup weight is over than target
+        if self.cup_weight >= self.target_weight:
+            self.done = True
+            if self.verbose:
+                print(
+                    "cup={}, step=(}, weight={}, reward={}".format(self.episode_count, self.step_count, self.cup_weight,
+                                                                   self._get_reward()))
+        # Replace the next source when the previous ice-cream is empty
+        if is_verifiers.count(True) < 1:
+            self.pipeline = self.__replace()
+        return self._get_obs(), self._get_reward(), self.done, self._get_info()
 
     def show(self):
         colors = matplotlib.pyplot.cm.rainbow
-        matplotlib.pyplot.imshow(self.ice_cream, interpolation='nearest', cmap=colors)
+        matplotlib.pyplot.figure()
+        matplotlib.pyplot.imshow(self.pipeline, interpolation='nearest', cmap=colors)
         matplotlib.pyplot.title('Ice-cream')
         matplotlib.pyplot.show(block=False)
         matplotlib.pyplot.pause(0.5)
@@ -67,19 +138,21 @@ class Simulator:
 
 # Test the simulator
 if __name__ == "__main__":
+    target = 40.
     source = './sample/Icecream.csv'
     tool = './sample/Scoop.csv'
-    sim = Simulator(source, tool)
-    no = 0
+    sim = Simulator(target, source, tool)
+    count = 100
     print("Ready to the simulator")
     time.sleep(2)
-    while True:
-        points = sim.inspection()
-        if len(points) == 0:
-            break
-        pos = random.choice(points)
-        sim(pos)
-        matplotlib.pyplot.figure()
-        sim.show()
-        print("Job {}, Weight {}".format(no, sim.weight))
-        no += 1
+    for i in range(count):
+        sim.reset()
+        done = False
+        while not done:
+            action = random.randrange(0, sim.cmd_count + 1)
+            obs, reward, done, info = sim.step([action])
+            if done:
+                print(
+                    "cup={}, step={}, weight={}, reward={}".format(i, info['step_count'], info['total_weight'], reward))
+                sim.show()
+                break
