ff1ae48185d7238ac3564cdac87bc36d78c3bd02
maroomir <maroomir@gmail.com>
2021-11-30 01:25:20 +0900

    Add the PPO and PD3 algorithm with argparser

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
+import argparse
 import re
 import time
 from glob import glob
 
 import torch.cuda
-from stable_baselines3 import DDPG
+from stable_baselines3 import DDPG, PPO, TD3
 
 from utils.callbacks import logDir
 from simulator import Simulator
@@ -11,8 +12,9 @@ from simulator import Simulator
 
 def model_verify(env_: Simulator,
                  device,
-                 thresh_=100,
-                 episode_=100):
+                 algorithm,
+                 thresh_=50,
+                 episode_=50):
     bk_verbose = env_.verbose
     env_.verbose = False
     env_.verify = True
@@ -21,15 +23,22 @@ def model_verify(env_: Simulator,
     model_files.sort(key=lambda var: [int(x) if x.isdigit() else x for x in re.findall(r'[^0-9]|[0-9]+', var)])
     res_files = []
     for model_path in model_files:
-        _model = DDPG.load(model_path, device=device)
+        if algorithm == 'DDPG':
+            _model = DDPG.load(model_path, device=device)
+        elif algorithm == 'PPO':
+            _model = PPO.load(model_path, device=device)
+        elif algorithm == 'TD3':
+            _model = TD3.load(model_path, device=device)
+        else:
+            raise Exception(f"Not supported {algorithm} algorithm")
         env_.reset(hard=True)
         try:
             for _ in range(episode_):
-                _obs = env.reset()
+                _obs = env_.reset()
                 _done = False
                 while not _done:
                     _action, _states = _model.predict(_obs)
-                    _obs, _, _done, _ = env.step(_action)
+                    _obs, _, _done, _ = env_.step(_action)
             print(f"{model_path} verified completed")
             res_files.append(model_path)
         except:
@@ -40,33 +49,65 @@ def model_verify(env_: Simulator,
     return res_files
 
 
-if __name__ == "__main__":
-    target = 40.
-    source = './sample/Icecream.csv'
-    tool = './sample/Scoop.csv'
+def parse_opt():
+    parser = argparse.ArgumentParser(description='Select the test model')
+    parser.add_argument('--source', type=str, default='./sample/Icecream.csv', help='an ice-cream source path')
+    parser.add_argument('--tool', type=str, default='./sample/Scoop.csv', help='a scooping tool path')
+    parser.add_argument('--target', type=float, default=40., help='a target for one scoop')
+    parser.add_argument('--algorithm', type=str, default='DDPG', help='use algorithm (DDPG, PPO, TD3)')
+    parser.add_argument('--skip-verify', action='store_false', default=True, help='skip the model varify')
+    parser.add_argument('--skip-plot', action='store_false', default=True, help='skip the drawing ice-cream plot')
+    opt = parser.parse_args()
+    return opt
+
+
+def run(source: str, tool: str, target: float, algorithm: str, verify=True, show_plot=True):
     if torch.cuda.is_available():
         device = torch.device('cuda')
     else:
         device = torch.device('cpu')
     env = Simulator(target, source, tool, verbose=False)
-    verified_files = model_verify(env, device, thresh_=50, episode_=50)
-    if len(verified_files) == 0:
-        print("No models can be tested")
+    if verify:
+        verified_files = model_verify(env, device, algorithm)
+        if len(verified_files) == 0:
+            print("No models can be tested")
+            return
+        else:
+            print(f"Verified model = {verified_files}")
+            best_model = verified_files[-1]
     else:
-        print(f"Verified model = {verified_files}")
-        best_model = verified_files[-1]
+        model_files = sorted(glob(logDir() + '*_best_model.pkl'))
+        if len(model_files) == 0:
+            print("Any modules remain the path")
+            return
+        else:
+            best_model = model_files[-1]
+
+    if algorithm == 'DDPG':
         model = DDPG.load(best_model, device=device)
-        test_episode = 100
-        time.sleep(5)
-        print(f"Test ready to use {best_model}, episode={test_episode}")
-        for i in range(test_episode):
-            obs = env.reset()
-            done = False
-            while not done:
-                action, states = model.predict(obs)
-                obs, reward, done, info = env.step(action)
-                if done:
+    elif algorithm == 'PPO':
+        model = PPO.load(best_model, device=device)
+    elif algorithm == 'TD3':
+        model = TD3.load(best_model, device=device)
+    else:
+        raise Exception(f"Not supported {algorithm} algorithm")
+    test_episode = 100
+    time.sleep(5)
+    print(f"Test ready to use {best_model}, episode={test_episode}")
+    for i in range(test_episode):
+        obs = env.reset()
+        done = False
+        while not done:
+            action, states = model.predict(obs)
+            obs, reward, done, info = env.step(action)
+            if done:
+                print(
+                    "cup={}, step={}, weight={}, reward={}".format(i, info['step_count'], info['total_weight'], reward)
+                )
+                if show_plot:
                     env.show()
-                    print(
-                        "cup={}, step={}, weight={}, reward={}".format(i, info['step_count'], info['total_weight'],
-                                                                       reward))
+
+
+if __name__ == "__main__":
+    arg = parse_opt()
+    run(**vars(arg))
--- train.py
+++ train.py
@@ -1,17 +1,28 @@
+import argparse
+
 import numpy
 
 from simulator import Simulator
 from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.ddpg.policies import MlpPolicy
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
-from stable_baselines3 import DDPG
+from stable_baselines3 import DDPG, PPO, TD3
 from utils.callbacks import getBestRewardCallback, logDir
 
-if __name__ == "__main__":
-    target = 40.
-    source = './sample/Icecream.csv'
-    tool = './sample/Scoop.csv'
-    env = Simulator(target, source, tool, verbose=True)
+
+def parse_opt():
+    parser = argparse.ArgumentParser(description='Select the train model')
+    parser.add_argument('--source', type=str, default='./sample/Icecream.csv', help='an ice-cream source path')
+    parser.add_argument('--tool', type=str, default='./sample/Scoop.csv', help='a scooping tool path')
+    parser.add_argument('--target', type=float, default=40., help='a target for one scoop')
+    parser.add_argument('--time-steps', type=int, default=300000, help='total time steps')
+    parser.add_argument('--algorithm', type=str, default='DDPG', help='use algorithm (DDPG, PPO, TD3)')
+    parser.add_argument('--skip-verbose', action='store_false', default=True, help='skip the tracing')
+    opt = parser.parse_args()
+    return opt
+
+
+def run(source: str, tool: str, target: float, time_steps: int, algorithm: str, verbose=True):
+    env = Simulator(target, source, tool, verbose=verbose)
     env = Monitor(env, logDir(), allow_early_resets=True)
     callback = getBestRewardCallback()
     # the noise objects for DDPG
@@ -19,5 +30,22 @@ if __name__ == "__main__":
     param_noise = None
     action_noise = OrnsteinUhlenbeckActionNoise(mean=numpy.zeros(actions), sigma=float(0.5) * numpy.ones(actions))
     # construct the model
-    model = DDPG(MlpPolicy, env, verbose=1, action_noise=action_noise)
-    model.learn(total_timesteps=500000, log_interval=100, callback=callback)
+    if algorithm == 'DDPG':
+        from stable_baselines3.ddpg.policies import MlpPolicy
+        model = DDPG(MlpPolicy, env, verbose=1, action_noise=action_noise)
+        model.learn(total_timesteps=time_steps, log_interval=100, callback=callback)
+    elif algorithm == 'PPO':
+        from stable_baselines3.ppo.policies import MlpPolicy
+        model = PPO(MlpPolicy, env, verbose=1)
+        model.learn(total_timesteps=time_steps, log_interval=100, callback=callback)
+    elif algorithm == 'TD3':
+        from stable_baselines3.td3.policies import MlpPolicy
+        model = TD3(MlpPolicy, env, verbose=1)
+        model.learn(total_timesteps=time_steps, log_interval=100, callback=callback)
+    else:
+        raise Exception(f"Not supported {algorithm} algorithm")
+
+
+if __name__ == "__main__":
+    arg = parse_opt()
+    run(**vars(arg))
